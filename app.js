c31f6fa92559e89048d55ecb95682f7cf306684f
David Johnson <davidjohnson@Lees-MacBook-Pro.local>
2017-08-08 23:45:12 -0700

    Fixed errors and updated code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 alert('Welcome to Whose Developer is this Anyway? The guessing game where everything is made up and the points don\'t matter like the street signs to a cab driver!');
 
 var hometown = prompt('I am David Johnson and I am your host for today\'s show!\nYour first question to get to know me is do you know where I grew up?');
-console.log(name + ' conestant did/didn\'t know where David grew up.');
+console.log(hometown + ' the conestant did/didn\'t know where David grew up.');
 
 if (hometown.toLowerCase() === 'yes' || hometown.toLowerCase() === 'y') {
   alert('100,000 points due to your amazing ability to improvise and guess correctly.');
@@ -14,7 +14,7 @@ if (hometown.toLowerCase() === 'yes' || hometown.toLowerCase() === 'y') {
 }
 
 var school = prompt('Has David attended all these schools?\nDiving\nFlight\nCoding\nFire Fighting');
-console.log(school + ' contestant did/didn\'t know all the schools David attended');
+console.log(school + ' the contestant did/didn\'t know all the schools David attended.');
 if(school.toUpperCase() === 'YES' || school.toUpperCase() === 'Y'){
   alert('I like bunnies too! Congrats! 9,9999.2 points.');
 } else if (school.toUpperCase() === 'NO' || school.toUpperCase() === 'N') {
@@ -23,8 +23,8 @@ if(school.toUpperCase() === 'YES' || school.toUpperCase() === 'Y'){
   alert('Can\'t get away with it that easy!');
 }
 
-var travel = prompt('Has David traveled to?\nDubai\nSingapore\nGreece\nMexico');
-console.log(travel + ' contestant traveled to?\nDubai\nSingapore\nGreece\nMexico');
+var travel = prompt('Has David traveled to\nDubai\nSingapore\nGreece\nMexico');
+console.log(travel + ' David traveled to\nDubai\nSingapore\nGreece\nMexico');
 if
 (travel.toLowerCase() === 'yes' || travel.toLowerCase() === 'y'){
   alert('I used apple maps to get here! Congrats! 10,000 points.');
@@ -35,7 +35,7 @@ if
 }
 
 var occupation = prompt('Was I ever a ninja or a pirate?');
-console.log(occupation + 'was/was not a ninja or pirate.');
+console.log(occupation + ' he was/was not a ninja or pirate.');
 if (occupation.toUpperCase() === 'YES' || occupation.toUpperCase() === 'Y'){
   alert('Arrrrrghhhhh! 0 points');
 } else if (occupation.toUpperCase() === 'NO' || occupation.toUpperCase() === 'N'){
@@ -45,12 +45,12 @@ if (occupation.toUpperCase() === 'YES' || occupation.toUpperCase() === 'Y'){
 }
 
 var terms = prompt('Have I ever clicked \"i agree\" on the terms and conditions, without reading it?');
-console.log(terms + ' has/has\'nt clicked \"i agree\" on the terms and conditions, without reading it?');
+console.log(terms + ' he has/has\'nt clicked \"i agree\" on the terms and conditions, without reading it.');
 if
 (terms.toLowerCase() === 'yes' || 'y'){
   alert('You know I have! Haven\'t you!');
 } else if (terms.toLowerCase() === 'no' || terms.toLowerCase() === 'n'){
   alert('I agreed anyway!');
 } else{
-  alert('');
+  alert('I guess you enjoy reading them too');
 }

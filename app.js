7da1aa5c809c820b2db50aaf8335452d194859a3
David Johnson <davidjohnson@Lees-MacBook-Pro.local>
2017-08-08 18:38:32 -0700

    Worked on questions, added CSS, made index.html

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+alert('Welcome to Whose Developer is this Anyway? The guessing game where everything is made up and the points don\'t matter like the street signs to a cab driver!');
+
+var hometown = prompt('I am David Johnson and I am your host for today\'s show!\nYour first question to get to know me is did you know where I grew up?');
+console.log(name + ' conestant did/didn\'t know where David grew up.');
+if
+(name.toLowerCase === 'yes' || 'y') {
+  alert('100,000 points due to your amazing ability to improvise and guess correctly.');
+}
+else
+{
+  alert('Congratulations you recieve -1,000 points.');
+}
+
+var school = prompt('Has David attended all these schools?\nDiving\nFlight\nCoding\nFire Fighting');
+console.log(school + ' contestant did/didn\'t know all the schools David attended');
+if
+(school.toUpperCase === 'YES' || 'Y'){
+  alert('I like bunnies too! Congrats! 9,9999.2 points.');
+}
+else
+{
+  alert('I used apple maps to get here!');
+}
+
+var travel = prompt('Has David traveled to?\nDubai\nSingapore\nGreece\nMexico');
+console.log(travel + ' contestant traveled to?\nDubai\nSingapore\nGreece\nMexico');
+if
+(travel.toLowerCase() === 'yes' || 'y'){
+  alert('I used apple maps to get here! Congrats! 10,000 points.');
+}
+else
+{
+  alert('Whaaaaaaaat! 0 points!');
+}
+
+var occupation = prompt('Was I ever a ninja or a pirate?');
+console.log(occupation + 'was/was not a ninja or pirate.');
+if (occupation.toUpperCase() === 'YES' || occupation.toUpperCase() === 'Y'){
+  alert('Arrrrrghhhhh! 0 points');
+}
+else {
+  alert('50 points! Now help me my find my eye patch!');
+}
+
+var terms = prompt('Have I ever clicked \"i agree\" on the terms and conditions, without reading it?');
+console.log(terms + ' has/has\'nt licked \"i agree\" on the terms and conditions, without reading it?');
+if
+(terms.toLowerCase() === 'yes' || 'y'){
+  alert('');
+}
+else {
+  {
+    alert();
+  }
+}
